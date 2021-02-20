69a9a0b929ebc96c3fb70c9389e76be58849eba8
wlq <1763020198@qq.com>
2021-02-20 16:41:20 +0800

    增加了redis 以及redis的消息队列简单案例

---
+++ App/redis/controller/Index.php
@@ -0,0 +1,29 @@
+<?php
+namespace app\redis\controller;
+
+use extend\redis\Redis;
+
+class Index
+{
+
+    public function publisher()
+    {
+        $data = [rand(0,10),rand(0,10),rand(9,99),rand(0,10),rand(8,12)]; //这里可以是get或post请求过来的数据
+        $data = json_encode($data);
+        $redis = new Redis();
+        $in = $redis->lpush('queue',$data);
+        if($in) {
+            echo "入队成功";
+        }
+    }
+    public function customer()
+    {
+        //正常情况下 有定时脚本 定时获取消息去消费
+        $redis = new Redis();
+        $value = $redis->lpop('queue');
+        $value = json_decode($value,true);
+        print_r($value);
+        print_r($redis->lrange('queue',0,-1));
+    }
+}
+?>
---
+++ extend/redis/Redis.php
@@ -0,0 +1,150 @@
+<?php
+namespace extend\redis;
+
+use Predis\Client;
+
+
+/**
+
+ * \Redis
+
+ */
+
+class Redis
+
+{
+
+    protected static  $config = [
+       'host' => '127.0.0.1',
+       'port' => 6379,
+       'password' => 'foobared'
+   ];
+
+    protected static $redis;
+
+    public static function init()
+
+    {
+
+        self::$redis = new Client(self::$config);
+
+    }
+
+    public static function set($key,$value,$time=null,$unit=null)
+
+    {
+
+        self::init();
+
+        if ($time) {
+
+            switch ($unit) {
+
+                case 'h':
+
+                    $time *= 3600;
+
+                    break;
+
+                case 'm':
+
+                    $time *= 60;
+
+                    break;
+
+                case 's':
+
+                case 'ms':
+
+                    break;
+
+                default:
+
+                    throw new InvalidArgumentException('单位只能是 h m s ms');
+
+                    break;
+
+            }
+
+            if ($unit=='ms') {
+
+                self::_psetex($key,$value,$time);
+
+            } else {
+
+                self::_setex($key,$value,$time);
+
+            }
+
+        } else {
+
+            self::$redis->set($key,$value);
+
+        }
+
+    }
+
+    public static function get($key)
+
+    {
+
+        self::init();
+
+        return self::$redis->get($key);
+
+    }
+
+    public static function delete($key)
+
+    {
+
+        self::init();
+
+        return self::$redis->del($key);
+
+    }
+
+    public static function lpush($key,$value)
+    {
+
+        self::init();
+
+        return self::$redis->lpush($key,$value);
+
+    }
+    public static function lpop($key)
+    {
+
+        self::init();
+
+        return self::$redis->lpop($key);
+
+    }
+
+    public static function lrange($key,$start,$end)
+    {
+
+        self::init();
+
+        return self::$redis->lrange($key,$start,$end);
+
+    }
+
+    private static function _setex($key,$value,$time)
+
+    {
+
+        self::$redis->setex($key,$time,$value);
+
+    }
+
+    private static function _psetex($key,$value,$time)
+
+    {
+
+        self::$redis->psetex($key,$time,$value);
+
+    }
+
+}
+?>
